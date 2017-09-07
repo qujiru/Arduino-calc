763b885003eb5e467ed55159beadde4cf589a604
qujiru <qujiru@gmail.com>
2017-09-07 20:08:50 +0900

    add scripts

---
+++ script/imglcdnize.py
@@ -0,0 +1,99 @@
+import numpy as np
+from PIL import Image, ImageFilter, ImageChops, ImageOps
+import sys
+
+flag_ino_data = False 
+flag_senga = False
+
+def readoption(opt):
+    for c in opt:
+        if (c == 'i'):
+            print("write .ino data.")
+            global flag_ino_data
+            flag_ino_data = True
+        elif (c == 's'):
+            print("convert image to 'senbyou'")
+            global flag_senga
+            flag_senga = True
+        else:
+            if (c != '-'):
+                print("Invalid option: ", c)
+                print(" -i  : write .ino data")
+                print(" -s  : convert image to 'senbyou'")
+                quit()
+
+def shirokuro(img):
+    mimg = None
+    if flag_senga:
+        senga  = img.convert("L")
+        senga2 = senga.filter(ImageFilter.MaxFilter(5))
+        simg = ImageChops.difference(senga, senga2)
+        simg = ImageOps.invert(simg)
+        simg.thumbnail((128,48), Image.LANCZOS)
+        mimg = simg.convert("1")
+    else:
+        mimg = img.copy().quantize(64)
+        mimg.thumbnail((128,48), Image.LANCZOS)
+        mimg = mimg.convert("L").convert('1')
+    return mimg
+
+def lcdnize(img, imgx, imgy):
+    pages = []
+    pagenum = int(imgy / 8);
+    for p in range(0, pagenum):    #page
+        page = []
+        for x in range(0, imgx):        #col
+            binary = '0b'
+            for y in range(p * 8, p * 8 + 8)[::-1]:       #pixel
+                tmp = 1 if (img.getpixel((x, y)) == 0) else 0
+                binary = binary + str(tmp)
+            page.append(hex(int(binary, 2)))
+        pages.append(page)
+    return pages
+
+def save(pages, filename, imgx = 128):
+    f= open(filename, "w")
+    if flag_ino_data:
+        f.write("/* paste this file to your .ino file.*/\n")
+        f.write("//====== data start ======\n")
+    cnt = 0
+    for page in pages:
+        if flag_ino_data:
+            f.write("uint8_t page"+ str(cnt) + "["+ str(imgx)+ "] = ")
+            f.write('{')
+        for x in page:
+            f.write(x)
+            f.write(',')
+        if flag_ino_data:
+            f.write('};\n')
+        else:
+            f.write('\n')
+        cnt = cnt + 1
+    if flag_ino_data:
+        f.write("uint8_t *pages["+str(cnt)+"] = {")
+        for i in range(cnt):
+            f.write("page" + str(i) + ", ")
+        f.write("};\n")
+        f.write("//====== data end ======\n")
+    f.close()
+
+def main():
+    argv = sys.argv
+    argc = len(argv)
+    imagename = ""
+    if (argc < 2):
+        print("Usage: python imglcdnize.py sample.image -options")
+        quit()
+    else:
+        for option in argv:
+            if option[0] == '-':
+                readoption(option)
+    imgname = argv[1]
+    img = shirokuro(Image.open(imgname))
+    (imgx, imgy) = img.size
+    pages = lcdnize(img, imgx, imgy)
+    save(pages, imgname + ".lpi", imgx)
+
+
+if __name__ == '__main__':
+    main()
---
+++ script/moji.py
@@ -0,0 +1,39 @@
+from PIL import Image, ImageDraw, ImageFont
+import sys
+
+import imglcdnize
+
+def gen_image(char, size = (12, 16), fontname = "Minecraft.ttf", fontsize = 16):
+    img = Image.new('1', size, 'white')
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(fontname, fontsize)
+    draw.text((0, 0), char, font=font, fill='#000')
+    return img
+
+def img_proc(img):
+    img = img.quantize(4).convert('1')
+    return img
+
+def lcdnize(img, imgx, imgy):
+    pages = []
+    pagenum = int(imgy / 8);
+    for p in range(0, pagenum):    #page
+        page = []
+        for x in range(0, imgx):        #col
+            binary = '0b'
+            for y in range(p * 8, p * 8 + 8)[::-1]:       #pixel
+                tmp = 1 if (img.getpixel((x*2, y*2)) == 0) else 0
+                binary = binary + str(tmp)
+            page.append(hex(int(binary, 2)))
+        pages.append(page)
+    return pages
+
+def main():
+    for c in range(10):
+        img = gen_image(str(c))
+        (imgx, imgy) = img.size
+        dat =  lcdnize(img, int(imgx/2), int(imgy/2))
+        imglcdnize.save(dat, 'chars/'+str(c) + '.lpi')
+
+if __name__ == '__main__':
+    main()
